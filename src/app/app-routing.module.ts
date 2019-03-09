8867b8923555179ca5c1bd4e9df43825e1690cad
juan <jlopeze@uees.edu.ec>
2019-03-09 12:57:20 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './components/pages/dashboard/dashboard.component';
+import { LoginComponent } from './components/login/login.component';
+import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
+import { PagesComponent } from './components/pages/pages.component';
+import { RegistroComponent } from './components/login/registro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '',
+  component: PagesComponent,
+  children: [
+    {path: 'dashboard', component: DashboardComponent},
+    {path: '', pathMatch: 'full', redirectTo: '/dashboard'}
+  ]
+},
+  {path: 'login', component: LoginComponent},
+  {path: 'registro', component: RegistroComponent},
+  {path: '**', component: PagenotfoundComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
+import { DashboardComponent } from './components/pages/dashboard/dashboard.component';
+import { ProgressComponent } from './components/pages/progress/progress.component';
+import { GraficasComponent } from './components/pages/graficas/graficas.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { SidebarComponent } from './components/shared/sidebar/sidebar.component';
+import { PagesComponent } from './components/pages/pages.component';
+import { RegistroComponent } from './components/login/registro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    PagenotfoundComponent,
+    DashboardComponent,
+    ProgressComponent,
+    GraficasComponent,
+    HeaderComponent,
+    SidebarComponent,
+    PagesComponent,
+    RegistroComponent
   ],
   imports: [
     BrowserModule,
