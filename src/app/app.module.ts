49157066c5e094f1624f01398160b36ef58e49dd
juan <jlopeze@uees.edu.ec>
2019-03-11 22:24:59 -0500

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
+//modulos
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { PagesModule } from './components/pages/pages.module';
+//rutas
 import { AppRoutingModule } from './app-routing.module';
+
+//servicios
+
+
+//componentes
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
-import { DashboardComponent } from './components/pages/dashboard/dashboard.component';
-import { ProgressComponent } from './components/pages/progress/progress.component';
-import { GraficasComponent } from './components/pages/graficas/graficas.component';
-import { HeaderComponent } from './components/shared/header/header.component';
-import { SidebarComponent } from './components/shared/sidebar/sidebar.component';
-import { PagesComponent } from './components/pages/pages.component';
 import { RegistroComponent } from './components/login/registro.component';
 
 @NgModule({
@@ -18,16 +21,11 @@ import { RegistroComponent } from './components/login/registro.component';
     AppComponent,
     LoginComponent,
     PagenotfoundComponent,
-    DashboardComponent,
-    ProgressComponent,
-    GraficasComponent,
-    HeaderComponent,
-    SidebarComponent,
-    PagesComponent,
     RegistroComponent
   ],
   imports: [
     BrowserModule,
+    PagesModule,
     AppRoutingModule
   ],
   providers: [],
